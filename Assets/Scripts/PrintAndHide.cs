210ab24a9f809038a28830ca4bc1d69abe1ce001
Anzodroid <anzodroid@gmail.com>
2021-08-25 18:31:01 +1000

    100 percent complete

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -7,18 +7,31 @@ public class PrintAndHide : MonoBehaviour
     public Renderer rend;
     
     private int i;
-    
+    private int random;
+ 
     
     // Start is called before the first frame update
     void Start()
     {
         i = 3;
+        random = Random.Range(200, 251);
     }
 
     // Update is called once per frame
     void Update()
     {
         i++;
+        //Debug.Log("random: " + random);
         Debug.Log(this.gameObject.name+":"+i); 
+        if (this.gameObject.tag == "Red" && i == 100)
+        {
+            this.gameObject.SetActive(false);
+        }
+
+        if (this.gameObject.tag == "Blue" && i == random)
+        {
+            rend.enabled = false;
+        }
+
     }
 }
